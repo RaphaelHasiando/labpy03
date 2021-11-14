d3e321deffd8dcc3f19e3c523224a4a5c68ed2fe
Raphael Hasiando <61907877+Yeast-Beast@users.noreply.github.com>
2021-11-14 23:56:30 +0700

    Update latihan1.py

--- latihan1.py
+++ latihan1.py
@@ -1,8 +1,8 @@
 import random
 
-N = int(input("Masukkan nilai N: "))
+N = int(input("Masukkan nilai N = "))
 
-for i in range(1, N + 1):
+for x in range(1, N + 1):
     a = random.uniform(0.0,0.5)
-    print(f"Data ke {i} -> ", a)
+    print(f"Data ke {x} -> ", a)
 print("Selesai")
