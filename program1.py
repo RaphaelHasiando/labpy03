f000b8c825aa995718419871c1742496e81587ab
Raphael Hasiando <61907877+Yeast-Beast@users.noreply.github.com>
2021-11-15 08:59:01 +0700

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,16 @@
 modal_awal = 100000000
-hari = 0
-while hari < 8:
-    hari += 1
-    modal_awal = modal_awal 
-    print("Hari ke", hari, modal_awal)
+bulan = 0
+keuntungan = int()
+total_keuntungan = int()
+while bulan < 8:
+    bulan += 1
+    if bulan == 3:
+        keuntungan = modal_awal * 1/100
+    if bulan == 5:
+        keuntungan = modal_awal * 5/100
+    if bulan == 8:
+        keuntungan = modal_awal * 2/100
+    total_keuntungan += keuntungan
+    print(f"Bulan ke-{bulan} =", keuntungan)
+
+print("Total laba adalah", total_keuntungan)
